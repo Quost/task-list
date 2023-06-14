108f6cd909ab6147245adaa49c5bcf9f65da69ef
Matheus Quost <matheusquost@gmail.com>
2023-06-14 03:41:33 -0300

    📚 Improving manifest items.

--- src/components/not-found/NotFound.jsx
+++ src/components/not-found/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
-  return <h1>Página não encontrada</h1>;
+  return (
+    <div className="not-found">
+      <img src="https://www.altinbas.com/images/404.jpg" alt="404 Error" className="not-found__image" />
+      <h1 className="not-found__title">Oops! Page Not Found</h1>
+      <p className="not-found__description">We're sorry, but the page you requested could not be found.</p>
+      <a href="/" className="not-found__button">Go to Home</a>
+    </div>
+  );
 };
 
 export default NotFound;
